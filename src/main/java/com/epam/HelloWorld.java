9311e4e785f30b5f7d18fe3e29bc7217b1acf884
Oleksii Gerasymov <Oleksii_Gerasymov@epam.com>
2016-08-18 15:56:03 +0300

    Initial commit

---
+++ src/main/java/com/epam/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.epam;
+
+/**
+ * Created by Oleksii_Gerasymov on 8/15/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!!");
+    }
+}
---
+++ src/test/java/com/epam/LittleTest.java
@@ -0,0 +1,14 @@
+package com.epam;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LittleTest {
+
+    @Test
+    public void SimpleTest() {
+        int numberOne = 1;
+        int numberTwo = 2;
+        assertEquals(numberOne, numberTwo);
+    }
+}
